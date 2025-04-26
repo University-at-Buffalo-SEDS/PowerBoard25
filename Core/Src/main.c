c0f2c5c16a1d142e265576e471b951a3b28a6832
Parth Agrawal <parthnyc2005@gmail.com>
2025-04-25 20:23:20 -0400

    updated power board code to pull data from sensor, put it in a queuem, and send it over CAN

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -47,29 +47,30 @@ FDCAN_HandleTypeDef hfdcan2;
 
 I2C_HandleTypeDef hi2c2;
 
-UART_HandleTypeDef hlpuart1;
+UART_HandleTypeDef huart2;
 
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 700 * 4
-};
+/* Definitions for blinkLED */
+osThreadId_t blinkLEDHandle;
+const osThreadAttr_t blinkLED_attributes = {
+    .name = "blinkLED",
+    .priority = (osPriority_t)osPriorityNormal,
+    .stack_size = 700 * 4};
 /* Definitions for readVoltageTask */
 osThreadId_t readVoltageTaskHandle;
 const osThreadAttr_t readVoltageTask_attributes = {
-  .name = "readVoltageTask",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 700 * 4
-};
-/* Definitions for blinkLEDTask */
-osThreadId_t blinkLEDTaskHandle;
-const osThreadAttr_t blinkLEDTask_attributes = {
-  .name = "blinkLEDTask",
-  .priority = (osPriority_t) osPriorityLow,
-  .stack_size = 700 * 4
-};
+    .name = "readVoltageTask",
+    .priority = (osPriority_t)osPriorityNormal,
+    .stack_size = 700 * 4};
+/* Definitions for sendMessage */
+osThreadId_t sendMessageHandle;
+const osThreadAttr_t sendMessage_attributes = {
+    .name = "sendMessage",
+    .priority = (osPriority_t)osPriorityNormal,
+    .stack_size = 700 * 4};
+/* Definitions for sensorQueue */
+osMessageQueueId_t sensorQueueHandle;
+const osMessageQueueAttr_t sensorQueue_attributes = {
+    .name = "sensorQueue"};
 /* USER CODE BEGIN PV */
 
 FDCAN_RxHeaderTypeDef RxHeader;
@@ -84,10 +85,10 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_FDCAN2_Init(void);
 static void MX_I2C2_Init(void);
-static void MX_LPUART1_UART_Init(void);
-void StartDefaultTask(void *argument);
+static void MX_USART2_UART_Init(void);
+void StartBlink(void *argument);
 void startReadVoltageTask(void *argument);
-void startBlinkLEDTask(void *argument);
+void StartSendMessage(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -96,29 +97,26 @@ void startBlinkLEDTask(void *argument);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-void CDC_Transmit_Print(const char * format, ...) {
-	char buf[PRINT_BUFFER_SIZE];
-	va_list args;
-	va_start(args, format);
-	int n = vsprintf(buf, format, args);
-	va_end(args);
-	CDC_Transmit_FS(buf, n);
+void CDC_Transmit_Print(const char *format, ...)
+{
+  char buf[PRINT_BUFFER_SIZE];
+  va_list args;
+  va_start(args, format);
+  int n = vsprintf(buf, format, args);
+  va_end(args);
+  CDC_Transmit_FS(buf, n);
 }
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
-
-
-
 
   /* USER CODE END 1 */
 
@@ -142,50 +140,10 @@ int main(void)
   MX_GPIO_Init();
   MX_FDCAN2_Init();
   MX_I2C2_Init();
-  MX_LPUART1_UART_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
-  	  FDCAN_FilterTypeDef sFilterConfig;
-
-	/* Configure Rx filter */
-	sFilterConfig.IdType = FDCAN_STANDARD_ID;
-	sFilterConfig.FilterIndex = 0;
-	sFilterConfig.FilterType = FDCAN_FILTER_MASK;
-	sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
-	sFilterConfig.FilterID1 = 0x321;
-	sFilterConfig.FilterID2 = 0x7FF;
-	if (HAL_FDCAN_ConfigFilter(&hfdcan2, &sFilterConfig) != HAL_OK)
-	{
-	  CDC_Transmit_Print("Error while configuring filter for FDCAN2");
-	}
-
-	/* Configure global filter:
-	   Filter all remote frames with STD and EXT ID
-	   Reject non matching frames with STD ID and EXT ID */
-	if (HAL_FDCAN_ConfigGlobalFilter(&hfdcan2, FDCAN_REJECT, FDCAN_REJECT, FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE) != HAL_OK)
-	{
-		CDC_Transmit_Print("Error while configuring global filter\n");
-	}
-
-  HAL_StatusTypeDef err = HAL_FDCAN_Start(&hfdcan2);
-    if (err != HAL_OK) {
-  	  char buf[60];// to send
-  	  int n = sprintf(buf, "init err: = 0x%02x\n", err);
-  	  CDC_Transmit_FS(buf, n);
-    }
-
-    /* TEMPORARY TILL I FIGURE OUR LED DRIVER */
-    HAL_Delay(1000);
-
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
-
-
-
-
-    /* END OF TEMP FOR LED DRIVER */
-
-
-
+  HAL_FDCAN_Start(&hfdcan2);
 
   /* USER CODE END 2 */
 
@@ -204,19 +162,23 @@ int main(void)
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
+  /* Create the queue(s) */
+  /* creation of sensorQueue */
+  sensorQueueHandle = osMessageQueueNew(16, sizeof(uint16_t), &sensorQueue_attributes);
+
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  /* creation of blinkLED */
+  blinkLEDHandle = osThreadNew(StartBlink, NULL, &blinkLED_attributes);
 
   /* creation of readVoltageTask */
   readVoltageTaskHandle = osThreadNew(startReadVoltageTask, NULL, &readVoltageTask_attributes);
 
-  /* creation of blinkLEDTask */
-  blinkLEDTaskHandle = osThreadNew(startBlinkLEDTask, NULL, &blinkLEDTask_attributes);
+  /* creation of sendMessage */
+  sendMessageHandle = osThreadNew(StartSendMessage, NULL, &sendMessage_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -233,45 +195,8 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
-	//QUICK TEST FOR LEDS
-
-
   while (1)
   {
-//	  if(HAL_FDCAN_GetRxFifoFillLevel(&hfdcan2, FDCAN_RX_FIFO0) > 0) {
-//	  		  CDC_Transmit_Print("There are some messages in the buffer!\n"); //Data to send
-//	  		  //Recieve data
-//	  		  HAL_StatusTypeDef err = HAL_FDCAN_GetRxMessage(&hfdcan2, FDCAN_RX_FIFO0, &RxHeader, RxData);
-//	  		  if (err != HAL_OK)
-//	  		  {
-//	  			 // n = sprintf(printBuffer, );
-//	  			  CDC_Transmit_Print("Error recieving message: 0x%02x\n", err);
-//	  		  } else {
-//	  			  //n = sprintf(printBuffer, "Recieved message: %s", RxData);
-//	  			  //CDC_Transmit_FS(printBuffer, n);
-//	  			  CDC_Transmit_Print("Recieved message: %s\n", RxData);
-//	  		  }
-//	  	  } else {
-//	  		  CDC_Transmit_Print("NO MESSAGES IN FIFO0\n"); //Data to send
-//	  		  CDC_Transmit_Print("Current FDCAN state: 0x%02x\n", hfdcan2.State);
-//	  	  }
-
-
-	LTC2990_Step(&LTC2990_Handle);
-
-	float voltages[4];
-	LTC2990_Get_Voltage(&LTC2990_Handle, voltages);
-
-	for (int i = 0; i < 4; i++) {
-		CDC_Transmit_Print("Voltage %d: %d.%03d V\n", i + 1,  (int)voltages[i], (int)((voltages[i] - (int)voltages[i]) * 1000));;
-	}
-
-	 //HAL_Delay(500);
-	 HAL_GPIO_WritePin(GPIOB, FRONT_LED_Pin, GPIO_PIN_RESET);
-	 HAL_Delay(500);
-	 HAL_GPIO_WritePin(GPIOB, FRONT_LED_Pin, GPIO_PIN_SET);
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -281,22 +206,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI48;
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI48;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
@@ -307,9 +232,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -322,10 +246,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief FDCAN2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief FDCAN2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_FDCAN2_Init(void)
 {
 
@@ -361,14 +285,13 @@ static void MX_FDCAN2_Init(void)
   /* USER CODE BEGIN FDCAN2_Init 2 */
 
   /* USER CODE END FDCAN2_Init 2 */
-
 }
 
 /**
-  * @brief I2C2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2C2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2C2_Init(void)
 {
 
@@ -394,14 +317,14 @@ static void MX_I2C2_Init(void)
   }
 
   /** Configure Analogue filter
-  */
+   */
   if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Digital filter
-  */
+   */
   if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
   {
     Error_Handler();
@@ -409,66 +332,65 @@ static void MX_I2C2_Init(void)
   /* USER CODE BEGIN I2C2_Init 2 */
 
   /* USER CODE END I2C2_Init 2 */
-
 }
 
 /**
-  * @brief LPUART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_LPUART1_UART_Init(void)
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN LPUART1_Init 0 */
+  /* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END LPUART1_Init 0 */
+  /* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN LPUART1_Init 1 */
+  /* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END LPUART1_Init 1 */
-  hlpuart1.Instance = LPUART1;
-  hlpuart1.Init.BaudRate = 209700;
-  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
-  hlpuart1.Init.StopBits = UART_STOPBITS_1;
-  hlpuart1.Init.Parity = UART_PARITY_NONE;
-  hlpuart1.Init.Mode = UART_MODE_TX_RX;
-  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
   {
     Error_Handler();
   }
-  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+  if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN LPUART1_Init 2 */
+  /* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END LPUART1_Init 2 */
-
+  /* USER CODE END USART2_Init 2 */
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -477,10 +399,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, FRONT_LED_Pin|BACKLIGHT_LEDS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, FRONT_LED_Pin | BACKLIGHT_LEDS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : FRONT_LED_Pin BACKLIGHT_LEDS_Pin */
-  GPIO_InitStruct.Pin = FRONT_LED_Pin|BACKLIGHT_LEDS_Pin;
+  GPIO_InitStruct.Pin = FRONT_LED_Pin | BACKLIGHT_LEDS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -489,95 +411,97 @@ static void MX_GPIO_Init(void)
   /**/
   __HAL_SYSCFG_FASTMODEPLUS_ENABLE(SYSCFG_FASTMODEPLUS_PB9);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_StartBlink */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
+ * @brief  Function implementing the blinkLED thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartBlink */
+void StartBlink(void *argument)
 {
   /* init code for USB_Device */
   MX_USB_Device_Init();
   /* USER CODE BEGIN 5 */
   /* Infinite loop */
-  for(;;)
+  for (;;)
   {
-    osDelay(1);
+    HAL_GPIO_TogglePin(FRONT_LED_GPIO_Port, FRONT_LED_Pin);
+    osDelay(100);
   }
   /* USER CODE END 5 */
 }
 
 /* USER CODE BEGIN Header_startReadVoltageTask */
 /**
-* @brief Function implementing the readVoltageTask thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the readVoltageTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_startReadVoltageTask */
 void startReadVoltageTask(void *argument)
 {
   /* USER CODE BEGIN startReadVoltageTask */
-	LTC2990_Init(&LTC2990_Handle, &hi2c2);
+  MX_USB_Device_Init();
+  LTC2990_Init(&LTC2990_Handle, &hi2c2);
+  static const float multipliers[4] = {28.0f / 10.0f, 25.0f / 10.0f, 25.0f / 10.0f, 25.0f / 10.0f};
   /* Infinite loop */
-  for(;;)
+  for (;;)
   {
-	  LTC2990_Step(&LTC2990_Handle);
-	  float voltages[4];
-	  LTC2990_Get_Voltage(&LTC2990_Handle, voltages);
+    LTC2990_Step(&LTC2990_Handle);
+    float voltages[4];
+    LTC2990_Get_Voltage(&LTC2990_Handle, voltages);
 
-	  for (int i = 0; i < 4; i++) {
-		  CDC_Transmit_Print("Voltage %d: %d.%03d V\n", i ,  (int)voltages[i], (int)((voltages[i] - (int)voltages[i]) * 1000));;
-	  }
-	  osDelay(1);
+    for (int i = 0; i < 4; i++)
+    {
+      CDC_Transmit_Print("Voltage %d: %.4f V\r\n", i, voltages[i] * multipliers[i]);
+    }
+    osDelay(100);
   }
   /* USER CODE END startReadVoltageTask */
 }
 
-/* USER CODE BEGIN Header_startBlinkLEDTask */
+/* USER CODE BEGIN Header_StartSendMessage */
 /**
-* @brief Function implementing the blinkLEDTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_startBlinkLEDTask */
-void startBlinkLEDTask(void *argument)
+ * @brief Function implementing the sendMessage thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartSendMessage */
+void StartSendMessage(void *argument)
 {
-  /* USER CODE BEGIN startBlinkLEDTask */
+  /* USER CODE BEGIN StartSendMessage */
   /* Infinite loop */
-  for(;;)
+  for (;;)
   {
-	HAL_GPIO_WritePin(GPIOB, FRONT_LED_Pin, GPIO_PIN_SET);
-	osDelay(500);
-	HAL_GPIO_WritePin(GPIOB, FRONT_LED_Pin, GPIO_PIN_RESET);
-    osDelay(500);
+    osDelay(1);
   }
-  /* USER CODE END startBlinkLEDTask */
+  /* USER CODE END StartSendMessage */
 }
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM1 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM1) {
+  if (htim->Instance == TIM1)
+  {
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
@@ -586,9 +510,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -600,14 +524,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
